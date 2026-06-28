e9c897b319b231e2151b3fb4ececa5c10d18039a
gnaneshwarshanigarapu <shanigarapugnaneshwar143@gmail.com>
2026-06-28 11:20:08 +0530

    white color

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ScrollToTop from './components/ScrollToTop';
 import PublicPageLoaderProvider from './components/PublicPageLoader';
-import { toast } from 'sonner';
+import { Toaster } from 'sonner';
 import { AdminAuthProvider } from './admin/AdminAuthContext';
 import AdminRoute from './admin/AdminRoute';
 import AdminLayout from './admin/AdminLayout';
